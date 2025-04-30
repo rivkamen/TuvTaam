cfe980c59f267d0c4daed9f9606230391c34a841
rivkamen <159689577+rivkamen@users.noreply.github.com>
2025-04-30 23:24:12 +0300

    something is wrong with node moduls

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,15 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const UserController = require("../controller/userController")
+const UserController = require("../controllers/userController")
 const verifyJWT = require("../middleware/verifyJWT")
 
-// router.use(verifyJWT)
 router.get("/",verifyJWT, UserController.getUsers)
-router.post("/",verifyJWT,UserController.createUser)
 router.put("/:_id",verifyJWT, UserController.updateUser)
 router.delete("/:_id",verifyJWT, UserController.deleteUser)
 router.get("/:_id",verifyJWT, UserController.getUserById)
-router.get("/phone/:phone",verifyJWT, UserController.getUserByPhone)
-
 
 module.exports = router
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const mongoose=require("mongoose")
+const conectDB=require("./config/dbConn")
+const express=require("express")
+const cors=require("cors")
+const corsOptions=require("./config/corsOptions")
+const PORT=process.env.PORT||2023
+const app=express()
+conectDB()
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.static("public"))
+app.use('/api/users',require("./routes/UserRoute"))
+// app.use('/api/auth',require("./routes/auths"))
+// app.use('/api/functionToken',require("./middleware/verifyJWT"))
+
+
+
+app.get('*',(req,res)=>{
+    res.status(404).send("not defind")
+})
+
+app.post('*',(req,res)=>{
+    res.status(404).send("not defind")
+})
+mongoose.connection.once("open",()=>{
+    console.log("conect to mongoDB")
+    app.listen(PORT,()=>{
+        console.log(`running on port ${PORT}`)
+    })
+})
+mongoose.connection.on("error",(err)=>{
+    console.log("error")
+    console.log(err)
+})
