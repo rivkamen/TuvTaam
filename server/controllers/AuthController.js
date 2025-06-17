05716aab0043635fb9ad681d7864a946e2289019
shlmt <sh7618sh@gmail.com>
2025-06-17 20:24:46 +0300

    :wrench: update user model

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,76 +1,79 @@
-const User=require("../models/User")
-const bcrypt=require("bcrypt")
-const jwt= require('jsonwebtoken')
+const { User } = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-  console.log("login");
+	const { email, password } = req.body
+	console.log('login')
 
-  if (!email || !password) {
-    return res.status(400).json({ message: 'required field is missing' });
-  }
-  console.log("login");
-  
-  const adminEmail = process.env.ADMINEMAIL;
-  const adminPassword = process.env.ADMIN;
-  const adminUsername = process.env.ADMINUSERNAME;
+	if (!email || !password) {
+		return res.status(400).json({ message: 'required field is missing' })
+	}
+	console.log('login')
 
-  if (email === adminEmail && password === adminPassword) {
-    const adminInfo = {
-      username: adminUsername,
-      email: adminEmail,
-      role: 'admin'
-    };
-      console.log("login");
+	const adminEmail = process.env.ADMINEMAIL
+	const adminPassword = process.env.ADMIN
+	const adminUsername = process.env.ADMINUSERNAME
 
-    const token = jwt.sign(adminInfo, process.env.ACCESS_TOKEN_SECRET);
-    return res.json({ token , role: 'admin', usernane: adminUsername });
-  }
+	if (email === adminEmail && password === adminPassword) {
+		const adminInfo = {
+			username: adminUsername,
+			email: adminEmail,
+			role: 'admin'
+		}
+		console.log('login')
 
-  // בדיקת משתמש רגיל ממסד הנתונים
-  const user = await User.findOne({ email }).lean();
-  if (!user) {
-    return res.status(401).json({ message: "unauthorized" });
-  }
-  console.log("login");
+		const token = jwt.sign(adminInfo, process.env.ACCESS_TOKEN_SECRET)
+		return res.json({ token, role: 'admin', usernane: adminUsername })
+	}
 
+	// בדיקת משתמש רגיל ממסד הנתונים
+	const user = await User.findOne({ email }).lean()
+	if (!user) {
+		return res.status(401).json({ message: 'unauthorized' })
+	}
+	console.log('login')
 
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) {
-    return res.status(401).json({ message: "unauthorized" });
-  }
+	const match = await bcrypt.compare(password, user.password)
+	if (!match) {
+		return res.status(401).json({ message: 'unauthorized' })
+	}
 
-  const userInfo = {
-    _id: user._id,
-    username: user.username,
-    email: user.email,
-    role: user.role 
-  };
-  console.log(userInfo);
-  
-  const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET);
-  return res.json({ token, username: user.username});
-};
+	const userInfo = {
+		_id: user._id,
+		username: user.username,
+		email: user.email,
+		role: user.role,
+		parashah: user.parashah,
+		haftarah: user.haftarah,
+		dueDate: user.dueDate
+	}
+	console.log(userInfo)
 
-
-const register=async(req,res)=>{
-    console.log("register")
-    const {username,password,email} = req.body
-    if (!email || !password) {
-        return res.status(400).json({message:'required field is missing'})
-        }
-     const duplicate=await User.findOne({email}).lean()
-       if(duplicate)
-          return res.status(409).json({message:"duplicate email"})
-    const hashedPwd = await bcrypt.hash(password, 10)
-    const userObject= {username,password:hashedPwd,email}
-    const user = await User.create(userObject)
-    if(user){
-       return res.status(201).json({success:true,
-            message:`user ${user.username} created successfuly`,
-            })
-    }
-    else
-        return res.status(400).json({message:"failed"})
+	const token = jwt.sign(userInfo, process.env.ACCESS_TOKEN_SECRET)
+	return res.json({ token, user: userInfo })
 }
-module.exports={login,register}
+
+const register = async (req, res) => {
+	console.log('register')
+	const { username, password, email, dueDate, parashah, haftarah } = req.body
+	if (!email || !password || !dueDate || !parashah) {
+		return res.status(400).json({ message: 'required field is missing' })
+	}
+	const duplicate = await User.findOne({ email }).lean()
+	if (duplicate) return res.status(409).json({ message: 'duplicate email' })
+	const hashedPwd = await bcrypt.hash(password, 10)
+	const userObject = {
+		username: username ?? email.split('@')[0],
+		password: hashedPwd,
+		email,
+		dueDate,
+		parashah,
+		haftarah
+	}
+	const user = await User.create(userObject)
+	if (user) {
+		return res.status(201).json({ success: true, message: `user ${user.username} created successfuly` })
+	} else return res.status(400).json({ message: 'failed' })
+}
+module.exports = { login, register }
--- server/models/User.js
+++ server/models/User.js
@@ -1,30 +1,56 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-   username: { 
-            type: String, 
-             },
-    email: { 
-            type: String, 
-            required: true,
-            unique:true
+const TanakhRef = new Schema({
+	book: String,
+	startChapter: { type: String, required: true },
+	startVerse: { type: String, required: true },
+	endChapter: { type: String, required: true },
+	endVerse: { type: String, required: true }
+})
 
+const userSchema = new Schema(
+	{
+		username: {
+			type: String
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		role: {
+			type: String,
+			enum: ['admin', 'user'],
+			default: 'user',
+			required: true
+		},
+		dueDate: {
+			type: Date,
+			required: true
+		},
+		parashah: {
+			type: TanakhRef,
+			require: true
+		},
+		haftarah: {
+			type: TanakhRef
+		},
+		recordBookmark: {
+			type: Number,
+			default: 0
+		}
+	},
+	{
+		timestamps: true
+	}
+)
 
-            },
-    password: { 
-                type: String, 
-                required: true,
-              },
-    role:{
-      type: String, enum: ["admin", "user"], default: "user", required: true
-    }
-      
-},
-    
-{
-    timestamps:true
-    });
-
-module.exports = mongoose.model('User', userSchema);
-
+module.exports = {
+	User: mongoose.model('User', userSchema),
+	TanakhRef
+}
